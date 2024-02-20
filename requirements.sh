26a3b0259d173f6a612f5df9ea69cc5b634f483e
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-02-20 01:21:35 -0300

    Fix:Adicionando dependencias em requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,11 @@
-echo "Installing Postgres driver..."
+echo "Getting Postgres driver..."
 go get github.com/lib/pq
 
 echo "Installing SQLC..."
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
+
+echo "Getting SQL package..."
+go get github.com/jackc/pgx/v5
 
 echo "Generating SQLC..."
 sqlc generate
