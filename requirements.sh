563f8c8e2ef55593a2dcf7d364b0ccdaf5363782
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-02-19 23:37:46 -0300

    Fix:Incluindo a instalacao do driver do postgres

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,7 @@
-echo "SQLC install..."
+echo "Installing Postgres driver..."
+go get github.com/lib/pq
+
+echo "Installing SQLC..."
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 
 echo "Generating SQLC..."
