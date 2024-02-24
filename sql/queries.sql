90829f70bf2fef79e9204a7191f307322636d08a
Gabriel Teiga <gabrielteiga99@gmail.com>
2024-02-23 23:14:25 -0300

    Novo schema e consultas do banco de dados

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,14 +1,26 @@
 -- name: CreateTask :exec
 INSERT INTO task (title, describe, is_completed, created_at, deadline, project_id) VALUES ($1,$2,$3,$4,$5,$6);
 
+-- name: DeleteTask :exec
+DELETE FROM task WHERE id = $1 AND project_id = $2;
+
 -- name: GetTask :one
 SELECT * FROM task WHERE id = $1;
 
 -- name: GetTasks :many
 SELECT * FROM task;
 
+-- name: GetTasksByProject :many
+SELECT * FROM task WHERE project_id = $1 ORDER BY is_completed;
+
+-- name: MarkTaskAsCompleted :exec
+UPDATE task SET is_completed = true WHERE id = $1 AND project_id = $2;
+
 -- name: CreateProject :exec
 INSERT INTO project (title, describe, created_at) VALUES ($1, $2, $3);
+
+-- name: DeleteProject :exec
+DELETE FROM project WHERE id = $1;
 
 -- name: GetProject :one
 SELECT * FROM project WHERE id = $1;
